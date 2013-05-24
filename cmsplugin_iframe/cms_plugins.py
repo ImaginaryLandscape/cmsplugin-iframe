ace557574181a5828c8ae654ac74106b5ecc3127
David Bertrand <dbertrand@imagescape.com>
2013-05-24 17:09:21 +0000

    Copied snippet icon src and made the plugin text enabled.

--- cmsplugin_iframe/cms_plugins.py
+++ cmsplugin_iframe/cms_plugins.py
@@ -2,11 +2,13 @@ from cms.plugin_base import CMSPluginBase
 from cms.plugin_pool import plugin_pool
 from models import IFrame
 from django.utils.translation import ugettext as _
+from django.conf import settings
 
 class IFramePlugin(CMSPluginBase):
     model = IFrame
     name = _("iFrame")
     render_template = "cmsplugin_iframe/iframe.html"
+    text_enabled = True
     
     def render(self, context, instance, placeholder):
         placeholder_width = context.get('width', None)
@@ -20,6 +22,10 @@ class IFramePlugin(CMSPluginBase):
                         'iframe_width':width,
                         'iframe_height':instance.height,
                         'placeholder':placeholder})
+
         return context
+
+    def icon_src(self, instance):
+              return settings.STATIC_URL + u"cms/images/plugins/snippet.png"
 
 plugin_pool.register_plugin(IFramePlugin)
